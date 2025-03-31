719f61c9399e538d5bb5981cb97c9c7957e904c4
samhpark1 <samhpark1@gmail.com>
2025-03-31 12:59:43 -0400

    Added info to footer

--- web/frontend/src/components/Footer.tsx
+++ web/frontend/src/components/Footer.tsx
@@ -1,8 +1,11 @@
 function Footer() {
 
     return (
-      <div>
-        <h1>Footer</h1>
+      <div className="bg-white h-1/2 text-black text-[0.6rem] flex flex-col justify-center items-center py-20 gap-5">
+        <p>© 2025 Samuel Park and Isaac Efrosman.</p>
+        <p>This project is open source under the MIT License.</p>
+        <p>You are free to use, modify, and distribute it with attribution.</p>
+        <p className="font-bold">The content on this site is for informational purposes only and does not constitute professional financial advice</p>
       </div>
     )
   }
